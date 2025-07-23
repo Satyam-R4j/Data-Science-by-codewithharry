cc27ec15e010432c7ee7b75fd75074db3bed8445
Satyam-R4j <satyamraj418@gmail.com>
2025-07-23 15:30:16 +0530

    Model Training

--- Practical ML using Scikit-learn/Project_housing.py
+++ Practical ML using Scikit-learn/Project_housing.py
@@ -3,7 +3,11 @@ from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import root_mean_squared_error
+from sklearn.model_selection import cross_val_score
 import pandas as pd
 import numpy as np
 
@@ -45,6 +49,45 @@ full_pipeline = ColumnTransformer([
 
 housing_prepared = full_pipeline.fit_transform(housing)
 print(housing_prepared)
+
+
+#Training the model
+
+#linear Regression model
+lin_reg = LinearRegression()
+lin_reg.fit(housing_prepared,housing_labels)
+lin_preds = lin_reg.predict(housing_prepared)
+lin_rmse = root_mean_squared_error(housing_labels, lin_preds)
+# print("The root mean square error for linear regression is ",lin_rmse)
+lin_rmses = -cross_val_score(lin_reg,housing_prepared,housing_labels,scoring="neg_root_mean_squared_error",cv=10)
+print(pd.Series(lin_rmse).describe())
+
+
+
+#Decision Tree model
+dec_reg = DecisionTreeRegressor()
+dec_reg.fit(housing_prepared,housing_labels)
+dec_preds = dec_reg.predict(housing_prepared)
+# dec_rmse = root_mean_squared_error(housing_labels, dec_preds)
+dec_rmses = -cross_val_score(dec_reg,housing_prepared,housing_labels,scoring="neg_root_mean_squared_error",cv=10)
+
+
+# print("The root mean square error for decision tree regression is ",dec_rmses)
+print(pd.Series(dec_rmses).describe())
+
+
+
+
+#Random forest  model
+Random_for_reg = RandomForestRegressor()
+Random_for_reg.fit(housing_prepared,housing_labels)
+Random_for_preds = Random_for_reg.predict(housing_prepared)
+Random_for_rmse = root_mean_squared_error(housing_labels, Random_for_preds)
+# print("The root mean square error for Random Forest regression is ",Random_for_rmse)
+Random_for_rmses = -cross_val_score(Random_for_reg,housing_prepared,housing_labels,scoring="neg_root_mean_squared_error",cv=10)
+print(pd.Series(Random_for_rmses).describe())
+
+
 
 
 
